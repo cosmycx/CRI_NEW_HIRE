acd6d4b86a91c117891a61585d40d393095c4aca
coss <cosmycx@gmail.com>
2015-11-10 10:32:15 -0500

    first commit original files

---
+++ README.md
@@ -0,0 +1,1 @@
+a new hire test
---
+++ cri-boilerplate/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Corporate Reports</title>
+	<link rel="stylesheet" href="_css/main.css">
+	
+	<!--FOR SITES THAT REQUIRE IE8 SUPPORT USE 1.8.3 instead of 2.1.4 -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+
+</head>
+<body>
+<?php include('_includes/header.php'); ?>
+
+
+</body>
+</html>
